50a8ed7b9a19ed1e6d65aee82899504969432b16
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-02 13:07:51 +0300

    Adding light

---
+++ src/subsytems/lighting/facades/LightSystem.js
@@ -0,0 +1,13 @@
+class LightSystem {
+    constructor() {
+        this.lightController = new LightController();
+        this.roomLightManager = new RoomLightManager(this.lightController);
+        this.sceneManager = new SceneManager(this.lightController);
+
+        console.log('Light system initialized');
+    }
+
+    addLight(lightId, room, isDimmable = true) {
+        return this.lightController.addLight(lightId, room, isDimmable);
+    }
+}
