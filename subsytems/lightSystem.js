96c68f78e5cff8460ff85e4a30bcfe177db39a7c
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-02 11:11:49 +0300

    turning light on in a room

---
+++ subsytems/lightSystem.js
@@ -0,0 +1,19 @@
+class Light {
+    constructor(lightId, room) {
+        this.lightId = lightId;
+        this.room = room;
+        this.isOn = false;
+        this.brightness = 0;
+        this.color = "white";
+        this.isDimmable = false;
+    }
+
+    turnOn(){
+        this.isOn = true;
+        if (this.brightness === 0) {
+            this.brightness = 100;
+        }
+
+        console.log(`Light ${this.lightId} in {this.room} turned ON`);
+    }
+}
