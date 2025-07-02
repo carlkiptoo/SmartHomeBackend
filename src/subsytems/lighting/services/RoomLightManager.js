eac4fde371142209b7d0485b3d9b665f7fc7cc57
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-02 12:00:59 +0300

    turning on lights in a room

---
+++ src/subsytems/lighting/services/RoomLightManager.js
@@ -0,0 +1,11 @@
+class RoomLightManager {
+    constructor(lightController) {
+        this.lightController = lightController;
+    }
+
+    turnOnRoom(room) {
+        const roomLights = this.lightController.getLightsByRoom(room);
+        roomLights.forEach(light => light.turnOn());
+        console.log(`Lights in ${room} turned ON`);
+    }
+}
